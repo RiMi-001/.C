e826c27a12ad14c83fb369058e0b2ed41f685934
RicardoMil <ricmillercoelhodacosta@hotmail.com>
2026-05-05 08:01:56 -0300

    Add basic structure for product pricing calculation

--- exercício1AULA10.c
+++ exercício1AULA10.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
 
+int main(){
+	setlocale(LC_ALL,"Portuguese_Brazil");
+	float C_produto[10], V_produto[10], rentabilidade; 
+	int ordem = 1, i = 0;
+    while(ordem < 11){
+	printf("\nDigite o preço de compra da mercadoria %d: ", ordem);
+	scanf("%f", &C_produto[i]);
+	printf("Digite o preço de venda da mercadoria %d: ", ordem);
+    scanf("%f", &V_produto[i]);	
+	
+	rentabilidade = V_produto[i]/C_produto[i];
+	if()
+    
+	ordem++;
+	i++;
+	}
+	return 0;
+}
