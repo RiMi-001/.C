0a3a48200f94329aeba2a78e4733eec98ee59d6b
RicardoMil <ricmillercoelhodacosta@hotmail.com>
2026-05-05 08:29:25 -0300

    Enhance profitability categorization in product analysis
    Added categories for product profitability based on rentability thresholds.

--- exercício1AULA10.c
+++ exercício1AULA10.c
@@ -4,7 +4,7 @@
 int main(){
 	setlocale(LC_ALL,"Portuguese_Brazil");
 	float C_produto[10], V_produto[10], rentabilidade; 
-	int ordem = 1, i = 0;
+	int ordem = 1, i = 0, cat1 = 0, cat2= 0, cat3= 0;
     while(ordem < 11){
 	printf("\nDigite o preço de compra da mercadoria %d: ", ordem);
 	scanf("%f", &C_produto[i]);
@@ -12,10 +12,17 @@ int main(){
     scanf("%f", &V_produto[i]);	
 	
 	rentabilidade = V_produto[i]/C_produto[i];
-	if()
+	if(rentabilidade < 1.1){
+		cat1++;
+	}else if(rentabilidade >= 1.1 && rentabilidade <= 1.2){
+		cat2++;
+	}else{
+		cat3++;
+	}
     
 	ordem++;
 	i++;
 	}
+	printf("Produtos de baixa rentabilidade: %d\n\nProdutos de média rentabilidade: %d\n\nProdutos de alta rentabilidade: %d\n\n", cat1, cat2, cat3);
 	return 0;
 }
